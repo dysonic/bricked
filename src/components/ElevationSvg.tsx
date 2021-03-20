8f4a0e34940b1a662d1ba3da6c577e3d1119c301
Gareth Dyson <4315423+dysonic@users.noreply.github.com>
2021-03-20 14:00:17 +1300

    Scale brickwork ElevationSvg.

--- src/components/ElevationSvg.tsx
+++ src/components/ElevationSvg.tsx
@@ -3,6 +3,17 @@ import * as d3 from 'd3';
 import { Elevation } from '../utils/elevation';
 import { MORTAR_THICKNESS, BRICK_COLOR, MORTAR_COLOR } from '../constants';
 
+const drawBrickAndReturnNewX = (b: string, elevation: Elevation, x:number, y:number, svgContainer: SVGSVGElement): number => {
+  const path = d3.path();
+  const width = elevation.brickPalette[b];
+  path.rect(x, y, width, elevation.brick.height);
+  d3.select(svgContainer).append('path')
+    .style('stroke', 'none')
+    .style('fill', BRICK_COLOR)
+    .attr('d', path.toString());
+  return x + width + MORTAR_THICKNESS;
+};
+
 const drawElevation = (svgContainer: SVGSVGElement | null, elevation: Elevation) => {
   if (!svgContainer) {
     return;
@@ -16,30 +27,19 @@ const drawElevation = (svgContainer: SVGSVGElement | null, elevation: Elevation)
     .style('fill', MORTAR_COLOR)
     .attr('d', mortarPath.toString());
 
-  let i: number = elevation.numberOfCourses - 1;
+  let c: number = elevation.numberOfCourses - 1;
   let y: number = 0;
-  while (i >= 0) {
+  while (c >= 0) {
     let x:number = 0;
-    const bricks: Array<string> = elevation.courses[i].split('');
-    // const rectangle: HTMLElement = svgContainer.append('rect').attr("x", 10)
-    //   .attr("y", 10)
-    //   .attr("width", 50)
-    //   .attr("height", 100);
+    const bricks: Array<string> = elevation.courses[c].split('');
     bricks.forEach((b: string) => {
-      const path = d3.path();
-      const width = elevation.brickPalette[b];
-      path.rect(x, y, width, elevation.brick.height);
-      d3.select(svgContainer).append('path')
-        .style('stroke', 'none')
-        .style('fill', BRICK_COLOR)
-        .attr('d', path.toString());
-      x = x + width + MORTAR_THICKNESS;
+      x = drawBrickAndReturnNewX(b, elevation, x, y, svgContainer);
     });
 
-    y = y + elevation.verticalGauge[i].deltaHeight;
+    y = y + elevation.verticalGauge[c].deltaHeight;
 
     // i = 0;
-    i--;
+    c--;
   }
 };
 
@@ -56,7 +56,7 @@ export const ElevationSvg: FC<ElevationSvgProps> = ({ elevation }) => {
 
   // var svgContainer = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
   return (
-    <svg ref={svgEl} id={elevation.bond.id} className="elevation" width={elevation.width} height={elevation.height} />
+    <svg ref={svgEl} id={elevation.bond.id} className="elevation" width="500" viewBox={`0 0 ${elevation.width} ${elevation.height}`} />
   );
 };
 
