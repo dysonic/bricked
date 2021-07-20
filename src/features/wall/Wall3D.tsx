6ea5bb6105ef16779c761b63c9424265f99e7034
Gareth Dyson <4315423+dysonic@users.noreply.github.com>
2021-07-20 21:34:46 +1200

    Wall3D fixes. WIP.

--- src/features/wall/Wall3D.tsx
+++ src/features/wall/Wall3D.tsx
@@ -29,14 +29,19 @@ const createRectShape = (x: number, y: number, width: number, height: number): T
 
   return rect;
 }
-const getWallGeometry = (options: GetWallGeometryOptions): any => {
-  const { wall, width, height, depth } = options;
+const createWallGeometry = (wall: Wall): any => {
+
+  // One unit in three.js is one meter!
+  const widthUnits = getWallWidth(wall) / 1000;
+  const heightUnits = getWallHeight(wall) / 1000;
+  const depthUnits = wall.brickPalette.S / 1000; // double thickness
 
   // Wall
-  const wallShape = createRectShape(0, 0, width, height);
+  const wallShape = createRectShape(0, 0, widthUnits, heightUnits);
 
   // Add brick gaps (holes) to wall shape
-  const gapHeight = wall.brickDimension.height + MORTAR_THICKNESS;
+  const gapHeight = (wall.brickDimension.height + MORTAR_THICKNESS);
+  const gapUnitHeight = gapHeight / 1000;
   wall.courses.forEach((course: string, i: number) => {
     const cn: number = i + 1;
     const y: number = getCourseHeight(cn, wall.coursingChart);
@@ -44,11 +49,11 @@ const getWallGeometry = (options: GetWallGeometryOptions): any => {
     course.split('').forEach(brickLetter => {
       const brickWidth = wall.brickPalette[brickLetter.toUpperCase()];
       if (isGap(brickLetter)) {
-        const gapX = x - MORTAR_THICKNESS;
-        const gapY = y - gapHeight;
-        const gapWidth = brickWidth + (2 * MORTAR_THICKNESS);
-        // console.log(`gap C${cn} - (x,y,w,h): ${gapX}, ${gapY}, ${gapWidth}, ${gapHeight}`);
-        const gapShape = createRectShape(gapX, gapY, gapWidth, gapHeight);
+        const gapUnitX = (x - MORTAR_THICKNESS) / 1000;
+        const gapUnitY = (y - gapHeight) / 1000;
+        const gapUnitWidth = (brickWidth + (2 * MORTAR_THICKNESS)) / 1000;
+        console.log(`gap C${cn} - (x,y,w,h): ${gapUnitX}, ${gapUnitY}, ${gapUnitWidth}, ${gapUnitHeight}`);
+        const gapShape = createRectShape(gapUnitX, gapUnitY, gapUnitWidth, gapUnitHeight);
         wallShape.holes.push(gapShape);
         // const gapGeometry = new THREE.ShapeGeometry(gapShape);
         // geometry.merge(gapGeometry);
@@ -59,19 +64,16 @@ const getWallGeometry = (options: GetWallGeometryOptions): any => {
 
   const extrudeSettings = {
     bevelEnabled: false,
-    depth,
+    depth: depthUnits,
   }
   const geometry = new THREE.ExtrudeGeometry(wallShape, extrudeSettings );
-
-  // const geometry = new THREE.ShapeGeometry(wallShape);
   geometry.center();
-
 	return geometry;
 }
 
 const calculateCameraZPosition = (fovDegrees: number, halfWallWidth: number, halfWallHeight: number): number => {
   const a = (fovDegrees / 2) * Math.PI / 180;
-  const s = 1.2 * halfWallHeight;
+  const s = (1.2 * halfWallHeight) / 1000;
   const z = s / Math.tan(a);
   return Math.ceil(z);
 };
@@ -82,16 +84,23 @@ const renderWall = (canvas: HTMLCanvasElement | null, wall: Wall) => {
     return;
   }
 
+  const canvasWidth = canvas.offsetWidth;
+  const canvasHeight = canvas.offsetHeight;
+  console.log(`canvas dimensions: ${canvasWidth}x${canvasHeight}`);
+
   const width = getWallWidth(wall);
   const height = getWallHeight(wall);
   const depth = wall.brickPalette.S; // double thickness
-  console.log(`wall - (width, height, depth):', ${width}, ${height}, ${depth}`);
+  console.log(`wall - (width, height, depth):, ${width}, ${height}, ${depth}`);
   const halfWidth = width / 2;
   const halfHeight = height / 2;
   const halfDepth = depth / 2;
 
   // Renderer
-  const renderer = new THREE.WebGLRenderer({canvas});
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( canvasWidth, canvasHeight );
+  renderer.outputEncoding = THREE.sRGBEncoding;
 
   // Camera
   const fov = 75; // field-of-view (degrees)
@@ -102,10 +111,16 @@ const renderWall = (canvas: HTMLCanvasElement | null, wall: Wall) => {
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   // camera.position.z = 2000;
   camera.position.z = cameraPositionZ;
+  // camera.position.set(0, halfHeight, cameraPositionZ);
   console.log('camera.position:', camera.position);
 
   // Scene
   const scene = new THREE.Scene();
+  // scene.background = new THREE.Color('red');
+
+  // Help!
+  const axesHelper = new THREE.AxesHelper( 5 );
+  scene.add( axesHelper );
 
   // Plane Geometry (Test Camera)
   // const planeGeometry = new THREE.PlaneGeometry(1000, 1000);
@@ -115,12 +130,7 @@ const renderWall = (canvas: HTMLCanvasElement | null, wall: Wall) => {
   // scene.add(plane);
 
   // Wall Geometry
-  const geometry = getWallGeometry({
-    wall,
-    width,
-    height,
-    depth,
-  });
+  const geometry = createWallGeometry(wall);
 
   // Material
   // The `MeshBasicMaterial` is not affected by lights.
@@ -130,19 +140,29 @@ const renderWall = (canvas: HTMLCanvasElement | null, wall: Wall) => {
   // const material = new THREE.MeshLambertMaterial({ color: 0xffffff })
   // const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
 
-  // Mesh
-  const mesh = new THREE.Mesh(geometry, material);
-  console.log('mesh.position:', mesh.position);
+  // Wall Mesh
+  const wallMesh = new THREE.Mesh(geometry, material);
+  console.log('mesh.position:', wallMesh.position);
+  scene.add(wallMesh);
+  // wallMesh.position.setX(-halfWidth);
 
-  const box = new THREE.Box3().setFromObject(mesh);
-  const size = new Vector3();
-  const centre = new Vector3();
-  box.getSize(size);
-  box.getCenter(centre);
-  console.log('mesh size:', box.min, box.max, size, centre);
+  const grid = new THREE.GridHelper( 200, 40, 0x000000, 0x000000 );
+  // grid.material.opacity = 0.2;
+  // grid.material.transparent = true;
+  // scene.add( grid );
 
-  // Add to scene
-  scene.add(mesh);
+  // const box = new THREE.Box3().setFromObject(mesh);
+  // const size = new Vector3();
+  // const centre = new Vector3();
+  // box.getSize(size);
+  // box.getCenter(centre);
+  // console.log('mesh size:', box.min, box.max, size, centre);
+
+  // Ground
+  const groundLength = width * 2;
+  const ground = new THREE.Mesh( new THREE.PlaneGeometry( groundLength, groundLength ), new THREE.MeshBasicMaterial( { color: 0x999999, depthWrite: false } ) );
+  // ground.rotation.x = - Math.PI / 2;
+  scene.add( ground );
 
   // Add light
   const color = 0xFFFFFF;
@@ -160,8 +180,9 @@ const renderWall = (canvas: HTMLCanvasElement | null, wall: Wall) => {
     const speed = .2;
     const rot = time * speed;
     // mesh.rotation.x = rot;
-    mesh.rotation.y = rot;
+    wallMesh.rotation.y = rot;
 
+    ground.rotation.x = rot;
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
